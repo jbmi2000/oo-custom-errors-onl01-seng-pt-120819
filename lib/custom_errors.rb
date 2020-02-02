47bbdec74af48683ae9f354442e7a538274f29a6
Jeffrey Marbury <jeff.marbury@gmail.com>
2020-02-02 01:52:36 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -8,7 +8,10 @@ class Person
   def get_married(person)
     self.partner = person
     if person.class != Person
-      raise PartnerError
+      begin
+       raise PartnerError
+     rescue PArtnerError => error
+       puts error.message
     else
     person.partner = self
   end
